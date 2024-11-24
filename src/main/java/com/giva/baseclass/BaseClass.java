ca48ad07986b1a33fc90b1de9539c5398c3d3774
kush27082000 <kushagrasoni27082000@gmail.com>
2024-11-24 22:05:42 +0530

    commit the initial read config.Properties file and their read code

--- src/main/java/com/giva/baseclass/BaseClass.java
+++ src/main/java/com/giva/baseclass/BaseClass.java
@@ -1,4 +1,32 @@
 package com.giva.baseclass;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.module.Configuration;
+import java.util.Properties;
+
 public class BaseClass {
+    public static Properties prop;
+    public static WebDriver driver;
+
+    @BeforeTest
+    public void loadConfig() {
+        try {
+        prop = new Properties();
+        FileInputStream ip = null;
+
+            ip = new FileInputStream(System.getProperty("user.dir") + "\\Configurations\\config.properties" );
+            prop.load(ip);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
